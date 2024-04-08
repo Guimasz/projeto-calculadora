9d5dece2d3f1f30fb560195e3e80b33a8d7a3035
gui-motas <monjogui@gmail.com>
2024-04-08 12:17:15 -0300

    Adicionada a logica de ERRO e tecla ON/OFF
    Foi adicionada a lógica do estado de ERRO e criada as estruturas de códigos responsáveis por permitir o botão de ligar e desligar a calculadora.

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ class Calculadora {
         this.numDisplay = '0';
         this.ptDecimal = false;
         this.operador = '';
-        this.numAnterior='';
+        this.numAnterior = '';
         this.estadoErro = false;
         this.memoria = 0;
-        
+        this.estadoLigada = false;
 
-        
+
+        if (this.estadoLigada == false) {
+            document.getElementById('display').innerHTML = '';
+            this.numDisplay = '';
+            this.ptDecimal = false;
+            this.operador = '';
+            this.numAnterior = '';
+            this.estadoErro = false;
+            this.memoria = 0;
+            this.estadoLigada = false;
+
+        }
+
     }
 
     display() {
@@ -21,54 +33,75 @@ class Calculadora {
         if (dig.length != 1) return;
         if (this.numDisplay.length == 10) return;
         if ((dig < '0' || dig > '9') && dig != '.') return;
-            if (this.numDisplay == '0') {
-                this.numDisplay = dig == '.' ? '0.' : dig;
-            } else {
-                this.numDisplay += dig;
-            }      
+        if (this.numDisplay == '0') {
+            this.numDisplay = dig == '.' ? '0.' : dig;
+        } else {
+            this.numDisplay += dig;
         }
-    
-    
+    }
+
+
 }
 
 //-----------------------------------------------------------------------------
 
 let calculadora = new Calculadora();
 
+function on_off() {
+    calculadora.estadoLigada = !calculadora.estadoLigada;
+    if (calculadora.estadoLigada) {
+        document.getElementById('display').innerHTML = '0';
+        calculadora.numDisplay = '0';
+        calculadora.ptDecimal = false;
+        calculadora.operador = '';
+        calculadora.numAnterior = '';
+        calculadora.estadoErro = false;
+        calculadora.memoria = 0;
+    } else {
+        document.getElementById('display').innerHTML = '';
+        calculadora.numDisplay = '';
+    }
+}
 
- function mostrarDisplay() {
+function mostrarDisplay() {
     document.getElementById('display').innerHTML = calculadora.display();
 }
 
 function addNumber(dig) {
+    if (calculadora.estadoLigada == false) return;
+    if (calculadora.estadoErro) return;
     calculadora.recebeDigito(dig);
     mostrarDisplay();
 }
 
-function apagarDig(){
+function apagarDig() {
     let count = calculadora.numDisplay.split("");
-    calculadora.numDisplay = calculadora.numDisplay.slice(0, -count); 
+    calculadora.numDisplay = calculadora.numDisplay.slice(0, -count);
     mostrarDisplay();
 }
 
 
 function addOperator(op) {
-    if (calculadora.operador != '') {
+    if (calculadora.estadoLigada == false) return;
+    if (calculadora.estadoErro) return;
+    if (calculadora.operador !== '') {
         calcularResultado();
-    }   
+    }
     if (this.estadoErro) return;
     calculadora.operador = op;
     calculadora.numAnterior = calculadora.numDisplay;
-    
+
     limparDisplay();
 }
 
-function calcularResultado(){
+function calcularResultado() {
+    if (calculadora.estadoLigada == false) return;
+    if (calculadora.estadoErro) return;
     if (calculadora.operador == '') return;
     let resultado;
     const num1 = parseFloat(calculadora.numAnterior);
     const num2 = parseFloat(calculadora.numDisplay);
-    switch(calculadora.operador){
+    switch (calculadora.operador) {
         case '+':
             resultado = num1 + num2;
             break;
@@ -79,6 +112,12 @@ function calcularResultado(){
             resultado = num1 * num2;
             break;
         case '/':
+            if (num2 == 0) {
+                calculadora.estadoErro = true;
+                calculadora.numDisplay = 'Erro';
+                mostrarDisplay();
+                return;
+            }
             resultado = num1 / num2;
             break;
         case '%':
@@ -86,6 +125,7 @@ function calcularResultado(){
 
             break;
     }
+
     calculadora.operador = '';
     calculadora.ptDecimal = false;
     calculadora.numAnterior = '';
@@ -93,41 +133,40 @@ function calcularResultado(){
     mostrarDisplay();
 }
 
-function limparDisplay(){
+function limparDisplay() {
     calculadora.numDisplay = '';
     mostrarDisplay();
 }
 
 function teclaMmais() {
-    
+    if (calculadora.estadoLigada == false) return;
+    if (calculadora.estadoErro) return;
+    if (calculadora.numDisplay == '') {
+        calculadora.estadoErro = true;
+        calculadora.numDisplay = 'Erro';
+        mostrarDisplay();
+        return;
+    }
     calculadora.memoria += parseFloat(calculadora.numDisplay);
     calculadora.numDisplay = calculadora.memoria;
     mostrarDisplay();
 }
 
 function teclaMmenos() {
-
+    if (calculadora.estadoLigada == false) return;
+    if (calculadora.estadoErro) return;
     calculadora.memoria -= parseFloat(calculadora.numDisplay);
     calculadora.numDisplay = calculadora.memoria;
     mostrarDisplay();
 }
 
 function teclaCLM() {
-    
+
     calculadora.memoria = 0;
 }
 
 function teclaRM() {
-   
+
     calculadora.numDisplay = calculadora.memoria;
     mostrarDisplay();
 }
-
-
-
-
-
-
-
-
-
