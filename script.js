0da9500081406977813371f151ef5dfaac82da84
Victor Liger <128982763+VicLiger@users.noreply.github.com>
2024-04-16 16:10:28 -0300

    Evitando que retorne NaN

--- script.js
+++ script.js
@@ -94,6 +94,17 @@ function calcularResultado() {
     if (calculadora.estadoLigada == false) return;
     if (calculadora.estadoErro) return;
     if (calculadora.operador == '') return;
+
+    if (calculadora.numDisplay === '') {
+        if (calculadora.numAnterior !== '') {
+            calculadora.numDisplay = calculadora.numAnterior;
+            mostrarDisplay();
+            return;
+        } else {
+            return;
+        }
+    }
+    
     let resultado;
     const num1 = parseFloat(calculadora.numAnterior);
     const num2 = parseFloat(calculadora.numDisplay);
